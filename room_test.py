7ef0914f9ab78de7dd7ffa62ce1fe8c8490163f4
August Ottens <ottensjacob@gmail.com>
2022-03-20 13:26:30 -0700

    changed code to be more like lab3

--- room_test.py
+++ room_test.py
@@ -5,12 +5,16 @@ import unittest
 from constants import *
 from room import ChatMessage, ChatRoom, MessageProperties, RoomList
 
-PRIVATE_ROOM_NAME = 'eshner'
+PRIVATE_ROOM_NAME = 'EJEA'
 PUBLIC_ROOM_NAME = 'general'
 SENDER_ALIAS = 'testing_sender'
 NON_MEMEBER_ALIAS = 'non-member'
-DEFAULT_PRIVATE_TEST_MESS = 'DE: Test Private Queue at '
-DEFAULT_PUBLIC_TEST_MESS = 'DE: Test Public Queue at '
+DEFAULT_PRIVATE_TEST_MESS = 'Test private'
+DEFAULT_PUBLIC_TEST_MESS = 'Test public'
+EMPTY_STRING = ""
+NUM_STRING = "12345"
+NEW_LINE = "Yo\nWhat's up\n\n"
+DIFFERENT_LANGUAGE_CHARACTERS = "త్వరిత గోధుమ నక్క సోమరి కుక్కపైకి దూకుతుంది"
 RECEIVER_ALIAS = 'testing_reciever'
 MEMBER_LIST = [SENDER_ALIAS, RECEIVER_ALIAS]
 
@@ -18,21 +22,59 @@ class RoomTest(TestCase):
     """ Docstring
     """
     def setUp(self) -> None:
-        self.test_public_room = ChatRoom(PUBLIC_ROOM_NAME, owner_alias=RECEIVER_ALIAS, room_type=ROOM_TYPE_PUBLIC, create_new=True)
-        self.test_private_room = ChatRoom(PUBLIC_ROOM_NAME, RECEIVER_ALIAS, ROOM_TYPE_PRIVATE, True)
+        self.test_public_room = ChatRoom(PUBLIC_ROOM_NAME, owner_alias=RECEIVER_ALIAS, 
+            room_type=ROOM_TYPE_PUBLIC, create_new=True)
+        self.test_private_room = ChatRoom(room_name=PRIVATE_ROOM_NAME, owner_alias=RECEIVER_ALIAS, 
+            room_type=ROOM_TYPE_PRIVATE, create_new=True)
 
     def test_send(self, private_message: str = DEFAULT_PRIVATE_TEST_MESS, public_message: str = DEFAULT_PUBLIC_TEST_MESS) -> bool:
         """ Testing the send message functionality
+        Sends standard test messages, an empty string, a number string, a string with newline characters, 
+        and a string with non-english characters
         """
-        message_props_public = MessageProperties(PUBLIC_ROOM_NAME, RECEIVER_ALIAS, SENDER_ALIAS, ROOM_TYPE_PUBLIC)
+        message_props_public = MessageProperties(PUBLIC_ROOM_NAME, RECEIVER_ALIAS, SENDER_ALIAS, MESS_TYPE_SENT)
+        message_props_private = MessageProperties(PRIVATE_ROOM_NAME, RECEIVER_ALIAS, SENDER_ALIAS, MESS_TYPE_SENT)
+
         sent = self.test_public_room.send_message(public_message, SENDER_ALIAS, message_props_public)
         self.assertTrue(sent)
+        sent = self.test_public_room.send_message(EMPTY_STRING, SENDER_ALIAS, message_props_public)
+        self.assertTrue(sent)
+        sent = self.test_public_room.send_message(NUM_STRING, SENDER_ALIAS, message_props_public)
+        self.assertTrue(sent)
+        sent = self.test_public_room.send_message(NEW_LINE, SENDER_ALIAS, message_props_public)
+        self.assertTrue(sent)
+        sent = self.test_public_room.send_message(DIFFERENT_LANGUAGE_CHARACTERS, SENDER_ALIAS, message_props_public)
+        self.assertTrue(sent)
         
+        sent = self.test_private_room.send_message(private_message, SENDER_ALIAS, message_props_private)
+        self.assertTrue(sent)
+        sent = self.test_private_room.send_message(EMPTY_STRING, SENDER_ALIAS, message_props_public)
+        self.assertTrue(sent)
+        sent = self.test_private_room.send_message(NUM_STRING, SENDER_ALIAS, message_props_public)
+        self.assertTrue(sent)
+        sent = self.test_private_room.send_message(NEW_LINE, SENDER_ALIAS, message_props_public)
+        self.assertTrue(sent)
+        sent = self.test_private_room.send_message(DIFFERENT_LANGUAGE_CHARACTERS, SENDER_ALIAS, message_props_public)
+        self.assertTrue(sent)
+        return sent
         
 
     def test_get(self) -> list:
         """ Testing the get messages functionality
         """
+        self.test_send()
+        message_objects, message_bodies, num_messages = self.test_public_room.get_messages(RECEIVER_ALIAS)
+        self.assertEqual(len(message_objects), len(message_bodies))
+        self.assertEqual(num_messages, len(message_bodies))
+        self.assertGreater(num_messages, 0)
+
+        self.test_send()
+        message_bodies, num_messages = self.test_public_room.get_messages(RECEIVER_ALIAS, return_objects=False)
+        self.assertEqual(num_messages, len(message_bodies))
+        self.assertGreater(num_messages, 0)
+
+        self.test_send()
+
         pass
 
     def test_full(self):
