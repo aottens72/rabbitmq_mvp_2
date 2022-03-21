1b149ca1397b055d183fffed700af436fa9af76d
Aphollier <59976596+Aphollier@users.noreply.github.com>
2022-03-20 17:17:47 -0700

    I think this is done

--- users.py
+++ users.py
@@ -22,23 +22,22 @@ class ChatUser():
                 'create_time': self.__create_time,
                 'modify_time': self.__modify_time
         }
+    
+    @property
+    def alias(self):
+        return self.__alias
         
 class UserList():
     """ List of users, inheriting list class
     """
     def __init__(self, list_name: str = DEFAULT_USER_LIST_NAME) -> None:
-        self.__user_list = list()
+        self.__user_list = {}
         self.__mongo_client = MongoClient('mongodb://34.94.157.136:27017/')
         self.__mongo_db = self.__mongo_client.detest
         self.__mongo_collection = self.__mongo_db.users    
-        if self.__restore() is True:
-#            raise Exception('No name and no document to restore')
-            self.__dirty = False
-        else:
-            self.__name = list_name
+        self.__name = list_name
+        if not self.__restore():
             self.__create_time = datetime.now()
-            self.__modify_time = datetime.now()
-            self.__dirty = True
     
     def to_dict(self):
         return {'list_name': self.__name, 'user_list': self.__user_list, 'create_time': self.__create_time, 'modify_time': self.__modify_time}
@@ -46,24 +45,43 @@ class UserList():
     def register(self, new_alias: str) -> ChatUser:
         """
         """
-        pass
+        new_user = ChatUser(alias=new_alias)
+        self.__user_list[new_alias] = new_user
+        self.__persist()
+        return new_user
 
     def get(self, target_alias: str) -> ChatUser:
-        pass
+        return self.__user_list.get(target_alias, None)
 
     def get_all_users(self) -> list:
-        return self.__user_list
+        return self.__user_list.values()
 
     def append(self, new_user: ChatUser) -> None:
-        pass
+        new_alias = new_user.to_dict()['alias']
+        self.__user_list[new_alias] = new_user
 
     def __restore(self) -> bool:
         """ First get the document for the queue itself, then get all documents that are not the queue metadata
         """
-        pass
+        user_list_metadata = self.__mongo_collection.find_one( { 'list_name': self.__name})
+        if user_list_metadata:
+            restore_user_names = user_list_metadata['user_names']
+            for user_name in restore_user_names:
+                user_data = self.__mongo_collection.find_one({ 'alias': user_name})
+                self.__user_list[user_name] = ChatUser(alias=user_data['alias'], create_time=user_data['create_time'], modify_time=user_data['modify_time'])
+            return True
+        return False
 
     def __persist(self):
         """ First save a document that describes the user list (name of list, create and modify times)
             Second, for each user in the list create and save a document for that user
         """
-        pass
+        metadata = {
+            'list_name': self.__name,
+            'user_names': self.__user_list.keys,
+            'create_time': self.__create_time,
+            'modify_time': datetime.now()
+        }
+        self.__mongo_collection.insert_one(metadata)
+        for user in self.__user_list.values():
+            self.__mongo_collection.insert_one(user.to_dict())
